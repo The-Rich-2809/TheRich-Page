12a8730fcd265073cbfaa84a83c8c626464bb145
Ricardo Prado Remigio <97078181+The-Rich-2809@users.noreply.github.com>
2025-09-21 15:33:28 -0600

    2do

--- TheRich-Page/Controllers/HomeController.cs
+++ TheRich-Page/Controllers/HomeController.cs
@@ -6,6 +6,13 @@ namespace TheRich_Page.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
